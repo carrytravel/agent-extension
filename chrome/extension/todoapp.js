8565502c500eb5e70c066afb19bdab98cf23cacc
Ernest Cheng <ernestcheng2@gmail.com>
2019-12-04 17:26:16 -0800

    deleted all functionality

--- chrome/extension/todoapp.js
+++ chrome/extension/todoapp.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from '../../app/containers/Root';
-import './todoapp.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "../../app/containers/Root";
+import "./todoapp.css";
 
-chrome.storage.local.get('state', (obj) => {
+chrome.storage.local.get("state", obj => {
   const { state } = obj;
-  const initialState = JSON.parse(state || '{}');
+  const initialState = JSON.parse(state || "{}");
 
-  const createStore = require('../../app/store/configureStore');
-
-  ReactDOM.render(
-    <Root store={createStore(initialState)} />,
-    document.querySelector('#root')
-  );
+  ReactDOM.render(<Root />, document.querySelector("#root"));
 });
